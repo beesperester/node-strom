ff39864d21afe6dd6ef3009966b8020e69720827
Bernhard Esperester <bernhard@esperester.de>
2020-03-13 08:39:28 +0100

    creates bundle for commit

--- src/lib/commit.js
+++ src/lib/commit.js
@@ -1,6 +1,24 @@
+import path from 'path'
+import { getRepositoryDirectory } from './repository'
 import { deserialize } from "./utilities/serialization"
 import { hashPath } from "./utilities/hashing"
 
+export const getCommitsDirectory = () => {
+	return 'objects'
+}
+
 export const getCommit = (filesystem) => (id) => {
-	return deserialize(filesystem.read(hashPath(id)))
+	return deserialize(filesystem.read(path.join(
+		getRepositoryDirectory(),
+		getCommitsDirectory(),
+		hashPath(id)
+	)))
+}
+
+export const createBundle = (filesystem) => {
+	return {
+		get: getCommit(filesystem),
+
+		getDirectory: getCommitsDirectory
+	}
 }
